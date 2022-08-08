bd9d582c04a332330c76badcdb97f2e923b22cde
yuu183 <y.toyama888@gmail.com>
2022-08-08 19:04:53 +0900

    Issue6修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,7 +36,7 @@ class CalendarsController < ApplicationController
       end
 
       wday_num = Date.today.wday# wdayメソッドを用いて取得した数値
-      if #「wday_numが7以上の場合」という条件式
+      if wday_num >= 7#「wday_numが7以上の場合」という条件式
         wday_num = wday_num -7
       end
 
